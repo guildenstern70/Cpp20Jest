43e6795bc445dcbf65a50d5e9ac83963d8f8b1df
Alessio Saltarin <alessiosaltarin@gmail.com>
2021-08-30 10:07:25 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#if USE_MODULES
+    import <iostream>;
+#else
+    #include <iostream>
+#endif
+
+int main()
+{
+    std::cout << "Alessio" << std::endl;
+    std::cout << "51 appena compiuti" << std::endl;
+    return 0;
+}
+
